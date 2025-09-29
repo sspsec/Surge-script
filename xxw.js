32914ab0836e20d068c01c301b14ee518004c180
刘桑 <142762749+sspsec@users.noreply.github.com>
2025-09-29 10:16:57 +0800

    Update xxw.js

--- xxw.js
+++ xxw.js
@@ -1,3 +1,11 @@
 let body = $response.body;
-body = body.replace(/\udbd3\udf38\u0020\udbcc\udddb/g, "4年");
-$done({ body });
+if (body) {
+  const regex = />学制：</th><td>(.*?)<\/td><th>/;
+  body = body.replace(regex, match => {
+    const content = match.replace(/\udbd3\udf38\u0020\udbcc\udddb/g, "4年");
+    return content;
+  });
+  $done({ body });
+} else {
+  $done();
+}
